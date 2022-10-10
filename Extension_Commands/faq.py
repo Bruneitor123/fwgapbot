aa8c238dc0dc1ed2520437cececf4440a908c359
Bruneitor123 <brunitoaas@gmail.com>
2022-10-10 00:52:24 -0400

    Preparing for faq command

---
+++ Extension_Commands/faq.py
@@ -0,0 +1,9 @@
+import discord
+
+#Prepare everything for the faq command:
+
+#   TODO
+#   -> Add a /faq command
+#   -> command will use paginators and dropdowns to select game, then show many answers
+#   -> Bot can forward people to tickets in case of no answer found.
+#   -> Suggestions can be added to the faq via another command (Will change pre-defined answers and they will be stored in the database)
